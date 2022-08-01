26a102ec9cbd89fc97105e010b1d29954b9b1466
Jakub Konrad Minkiewicz <32709219+jakubminkiewicz@users.noreply.github.com>
2022-08-01 19:24:52 +0100

    Create PrivateConstants.swift

---
+++ MarvelComics/Constants/PrivateConstants.swift
@@ -0,0 +1,16 @@
+//
+//  PrivateConstants.swift
+//  MarvelComics
+//
+//  Created by Jakub Minkiewicz on 01/08/2022.
+//
+
+import UIKit
+
+struct PrivateConstants {
+    
+  // insert your Marvel API keys here
+    static let privateKey = ""
+    static let publicKey = ""
+    
+}
